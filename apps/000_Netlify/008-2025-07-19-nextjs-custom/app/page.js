97e226c8e266fcf5900e6db1d63c80c7a85702d2
spencermx <138399420+spencermx@users.noreply.github.com>
2025-07-19 14:28:39 -0600

    added pages

--- apps/000_Netlify/008-2025-07-19-nextjs-custom/app/page.js
+++ apps/000_Netlify/008-2025-07-19-nextjs-custom/app/page.js
@@ -1,6 +1,3 @@
-
 export default function Home() {
-
-
-
+  return <a href="/page1">Go to Page1</a>;
 }
---
+++ apps/000_Netlify/008-2025-07-19-nextjs-custom/app/page1/page.js
@@ -0,0 +1,8 @@
+export default function Page1() {
+  return (
+    <div>
+      <h1>Welcome to my Page1</h1>
+      <a href="/">Go to Home</a>
+    </div>
+  );
+}
