b2bf6910a087be81b7c5af37d0e239d889f67244
spencermx <138399420+spencermx@users.noreply.github.com>
2025-07-20 13:45:55 -0600

    added link to vercel

--- apps/000_Netlify/009-2025-07-20-nextjs-custom/app/page.js
+++ apps/000_Netlify/009-2025-07-20-nextjs-custom/app/page.js
@@ -1,11 +1,13 @@
-
-export default function Home(){
-
+export default function Home() {
   return (
     <>
-    <p>Here is the home page thanks!</p>
-    <a href="/bork">bork page</a>
-    </> 
-  )
-
+      <p>Here is the home page thanks!</p>
+      <div>
+        <a href="/bork">bork page</a>
+      </div>
+      <div>
+        <a href="/vercel/getdata">Get Vercel Data</a>
+      </div>
+    </>
+  );
 }
