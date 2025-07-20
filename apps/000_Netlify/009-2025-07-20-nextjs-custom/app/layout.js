cab29ad7a27a040c788d153830ab9edc190d53a2
spencermx <138399420+spencermx@users.noreply.github.com>
2025-07-20 13:31:59 -0600

    basic nextjs app

---
+++ apps/000_Netlify/009-2025-07-20-nextjs-custom/app/layout.js
@@ -0,0 +1,11 @@
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+      <h1>root layout</h1>
+      {children}
+      </body>
+    </html>
+  );
+}
---
+++ apps/000_Netlify/009-2025-07-20-nextjs-custom/app/page.js
@@ -0,0 +1,11 @@
+
+export default function Home(){
+
+  return (
+    <>
+    <p>Here is the home page thanks!</p>
+    <a href="/bork">bork page</a>
+    </> 
+  )
+
+}
